a8546b728aba59b7bca8411019341cabf97a2a75
blockheron <64575353+blockheron@users.noreply.github.com>
2025-11-08 17:55:15 -0500

    Added features

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -31,3 +31,92 @@ function setLatestStory() {
     const latestStoryLink = document.getElementsByName("latestStoryButton");
     latestStoryLink[0].setAttribute("href", "stories/story-" + latestStory + ".html");
 }
+
+// dark, light, dyslexia themes
+let light = localStorage.getItem("light");
+let lightDys = localStorage.getItem("light-dys");
+let darkDys = localStorage.getItem("dark-dys");
+// var theme = 0;
+
+const enableLightMode = () => {
+    document.body.classList.add("light");
+    localStorage.setItem("light", "active")
+    // theme = 1;
+}
+
+const enableLightDysMode = () => {
+    document.body.classList.remove("light");
+    localStorage.setItem("light", null)
+    document.body.classList.add("light-dys");
+    localStorage.setItem("light-dys", "active")
+    // theme = 2;
+}
+
+const enableDarkDysMode = () => {
+    document.body.classList.remove("light-dys");
+    localStorage.setItem("light-dys", null)
+    document.body.classList.add("dark-dys");
+    localStorage.setItem("dark-dys", "active")
+    // theme = 3;
+}
+
+const enableDarkMode = () => {
+    document.body.classList.remove("dark-dys");
+    localStorage.setItem("dark-dys", null);
+}
+
+if (light === "active") enableLightMode();
+if (lightDys === "active") enableLightDysMode();
+if (darkDys === "active") enableDarkDysMode();
+
+function changeTheme() {
+    light = localStorage.getItem("light");
+    lightDys = localStorage.getItem("light-dys");
+    darkDys = localStorage.getItem("dark-dys");
+
+    if (light === "active") {
+        console.log("lightDys");
+        enableLightDysMode();
+    } else if (lightDys === "active") {
+        console.log("darkDys");
+        enableDarkDysMode();
+    } else if (darkDys === "active") {
+        console.log("dark");
+        enableDarkMode();
+    } else {
+        enableLightMode();
+        console.log("light");
+    }
+}
+
+function themeListener() {
+    const intervalID = setInterval(changeThemeIframe, 1);
+}
+
+function changeThemeIframe() {
+    light = localStorage.getItem("light");
+    lightDys = localStorage.getItem("light-dys");
+    darkDys = localStorage.getItem("dark-dys");
+
+    var theme = 0;
+    if (light === "active") theme = 1;
+    if (lightDys === "active") theme = 2;
+    if (darkDys === "active") theme = 3;
+
+    switch (theme) {
+        case 1:
+            document.body.classList.add("light");
+            break;
+        case 2:
+            document.body.classList.remove("light");
+            document.body.classList.add("light-dys");
+            break;
+        case 3:
+            document.body.classList.remove("light-dys");
+            document.body.classList.add("dark-dys");
+            break;
+        default:
+            document.body.classList.remove("dark-dys");
+            break;
+    }
+}
